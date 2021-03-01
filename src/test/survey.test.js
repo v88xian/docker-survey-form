3c7b4ff8f02fd58ac1810872d67a19d84916e79a
v88xian <ccasugay@village88.com>
2021-03-01 16:05:26 +0800

    removed console.log

--- src/test/survey.test.js
+++ src/test/survey.test.js
@@ -11,7 +11,7 @@ describe("Survey Model", function(){
         let surveyModel = new SurveyModel();
         let captcha = surveyModel.captcha;
         let result = surveyModel.verifyCaptchaInput(captcha);
-        console.log(123);
+        
         expect(result).to.equal("Success! Captcha input matched.");
     });
 
